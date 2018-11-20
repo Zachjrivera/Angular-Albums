9b06eb97bb44e83ea494c0298276991c17595eb1
Zachjrivera <Zachjrivera@gmail.com>
2018-11-20 09:02:54 -0500

    Album app

---
+++ src/app/productdetails/productdetails.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Subject } from 'rxjs';
+declare var $: any;
+
+@Component({
+  selector: 'app-productdetails',
+  templateUrl: './productdetails.component.html',
+  styleUrls: ['./productdetails.component.css']
+})
+export class ProductdetailsComponent implements OnInit {
+
+  @Input() list : Subject<any>;
+  fulldetails : any[];
+
+  constructor() { }
+
+  ngOnInit() {
+
+    this.list.subscribe(
+      detail => {
+        console.log(detail);
+        this.fulldetails = detail;    
+      })
+    }
+
+}
